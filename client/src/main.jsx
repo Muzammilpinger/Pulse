97e55fef220a1f0e3fd9286b3147093e1d59882f
Muzzi <Muzzi>
2026-08-30 14:49:15 +0500

    feat(call): add voice call UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import Chat from "./chat/Chat";
+import Call from "./call/Call";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Chat />
+    <Call room="general" />
   </React.StrictMode>
 );
